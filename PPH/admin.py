74633bc26e98e5cf1887b400effa62e693df582d
jeremy <devpyth@outlook.fr>
2024-01-03 19:48:13 +0100

    Initial

---
+++ PPH/admin.py
@@ -0,0 +1,67 @@
+from django.contrib import admin
+from .models import CustomUser, UserFunction, Supplier, TypeMatiere, \
+    UniteMesure, Forme, MatierePremiere, Liste, TypePrep, Formule, Composition, \
+    Catalogue, Voie, ParametresPrep, ParametresFormules
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'function', 'is_active', 'is_staff')
+    search_fields = ('username', 'email')
+    list_filter = ('is_active', 'is_staff', 'function')
+
+
+class UserFunctionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    search_fields = ('title',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(UserFunction, UserFunctionAdmin)
+admin.site.register(Supplier)
+
+@admin.register(TypeMatiere)
+class TypeMatiereAdmin(admin.ModelAdmin):
+    list_display = ['id','nom','logo']
+
+@admin.register(UniteMesure)
+class UniteMesureAdmin(admin.ModelAdmin):
+    list_display = ['nom']
+
+@admin.register(Forme)
+class FormeAdmin(admin.ModelAdmin):
+    list_display = ['nom', 'unite_mesure', 'unite_stock']
+
+@admin.register(Liste)
+class ListeAdmin(admin.ModelAdmin):
+    list_display = ['nom']
+
+@admin.register(Voie)
+class VoieAdmin(admin.ModelAdmin):
+    list_display = ['nom']
+
+@admin.register(TypePrep)
+class TypePrepAdmin(admin.ModelAdmin):
+    list_display = ['nom']
+
+@admin.register(ParametresPrep)
+class ParametresPrepAdmin(admin.ModelAdmin):
+    list_display = ['nom', 'unite']
+
+@admin.register(ParametresFormules)
+class ParametresFormulesAdmin(admin.ModelAdmin):
+    list_display = ['num_formule', 'parametre']
+
+@admin.register(MatierePremiere)
+class MatierePremiereAdmin(admin.ModelAdmin):
+    list_display = ['nom', 'type', 'forme', 'qté_cdt', 'fournisseur', 'prix', 'prix_unit', 'qté_stock', 'stock_mini', 'stockee', 'cde']
+
+@admin.register(Formule)
+class TypePrepAdmin(admin.ModelAdmin):
+    list_display = ['nom', 'type']
+
+@admin.register(Composition)
+class Composition(admin.ModelAdmin):
+    list_display = ['num_formule', 'matiere', 'qté', 'unite']
+
+@admin.register(Catalogue)
+class CatalogueAdmin(admin.ModelAdmin):
+    list_display = ['designation', 'code_fournisseur', 'cip', 'fournisseur', 'qté', 'unite']
---
+++ pphfront/src/store/modules/suppliers.js
@@ -0,0 +1,146 @@
+import api from '../../../api';
+
+const state = () => ({
+  suppliers: [],
+  showMenu: {},
+  expanded: {},
+  currentSupplier: null,
+});
+
+const getters = {
+  allSuppliers: (state) => state.suppliers,
+  showMenu: (state) => state.showMenu,
+  expanded: (state) => state.expanded,
+  currentSupplier: state => {
+    return state.currentSupplier;
+  }
+};
+
+const actions = {
+
+    async addSupplier({ dispatch }, formData) {
+      try {
+        await api.post('PPH/suppliers/', formData);
+        dispatch('notifications/showNotification', {
+          message: 'Fournisseur ajouté avec succès',
+          type: 'success'
+        }, { root: true });
+        return Promise.resolve();
+      } catch (error) {
+        dispatch('notifications/showNotification', {
+          message: 'Erreur lors de l\'ajout du fournisseur',
+          type: 'error'
+        }, { root: true });
+        return Promise.reject(error);
+      }
+    },
+
+  async fetchSupplierById(id) {
+    try {
+      const response = await api.get(`PPH/suppliers/${id}`);
+      return response;
+    } catch (error) {
+      console.error("Erreur lors de la récupération du fournisseur:", error);
+      throw error; // pour pouvoir le gérer dans le composant
+    }
+  },
+
+  async loadSuppliers({ commit, dispatch }) {
+    try {
+      const response = await api.get('/PPH/suppliers');
+      commit('SET_SUPPLIERS', response.data);
+    } catch (error) {
+      dispatch('notifications/showNotification', {
+        message: 'Erreur lors du chargement des fournisseurs',
+        type: 'error'
+      }, { root: true });
+      console.error(error);
+    }
+  },
+
+  async toggleActivation({ dispatch }, payload) {
+    const { supplierId, isActive } = payload;
+    try {
+      if (isActive) {
+        await api.patch(`/PPH/suppliers/${supplierId}/`, { is_activate: false });
+        dispatch('notifications/showNotification', {
+          message: 'Fournisseur désactivé',
+          type: 'success'
+        }, { root: true });
+      } else {
+        await api.patch(`/PPH/suppliers/${supplierId}/`, { is_activate: true });
+        dispatch('notifications/showNotification', {
+          message: 'Fournisseur activé',
+          type: 'success'
+        }, { root: true });
+      }
+      dispatch('loadSuppliers');
+    } catch (error) {
+      dispatch('notifications/showNotification', {
+        message: 'Erreur lors du changement d\'état',
+        type: 'error'
+      }, { root: true });
+      console.error(error);
+    }
+  },
+
+  async deleteSupplier({ dispatch }, supplierId) {
+    try {
+      await api.delete(`/PPH/suppliers/${supplierId}`);
+      dispatch('notifications/showNotification', {
+        message: 'Fournisseur supprimé',
+        type: 'success'
+      }, { root: true });
+      dispatch('loadSuppliers');
+    } catch (error) {
+      dispatch('notifications/showNotification', {
+        message: 'Erreur lors de la suppression',
+        type: 'error'
+      }, { root: true });
+      console.error(error);
+    }
+  },
+
+  toggleMenu({ commit }, id) {
+    commit('TOGGLE_MENU', id);
+  },
+
+  toggleInfo({ commit }, id) {
+    commit('TOGGLE_INFO', id);
+  },
+
+  getSupplier({ commit }, id) {
+    return api.get(`/PPH/suppliers/${id}`)
+    .then((response) => {
+      commit('SET_CURRENT_SUPPLIER', response.data);
+      return response.data;
+    });
+  },
+};
+
+
+const mutations = {
+  ADD_SUPPLIER(state, supplier) {
+    state.suppliers.push(supplier);  // Ajoute le nouveau fournisseur à la liste des fournisseurs dans le state
+  },
+  SET_SUPPLIERS(state, suppliers) {
+    state.suppliers = suppliers;
+  },
+  TOGGLE_MENU(state, id) {
+    state.showMenu[id] = !state.showMenu[id];
+  },
+  TOGGLE_INFO(state, id) {
+    state.expanded[id] = !state.expanded[id];
+  },
+  SET_CURRENT_SUPPLIER(state, supplier) {
+    state.currentSupplier = supplier;
+  }
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+};
