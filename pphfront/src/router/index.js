344939e07a36cbfc2c7c2ed475c705ea25d802cf
Jérémy <devpyth@outlook.fr>
2024-01-07 00:20:45 +0100

    Update index.js

--- pphfront/src/router/index.js
+++ pphfront/src/router/index.js
@@ -128,7 +128,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL || '/'),
+  history: createWebHistory(process.env.BASE_URL || '/'),
   routes
 })
 
