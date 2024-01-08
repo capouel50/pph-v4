27bb6fc1ab7ee3f268c6ca3dafbb94595963fc75
jeremy <devpyth@outlook.fr>
2024-01-08 09:04:09 +0100

    Deploiment ok en local

--- Procfile
+++ Procfile
@@ -4,3 +4,6 @@ web: gunicorn Hospi.wsgi --chdir Hospi
 # Pour le frontend Vue.js
 frontend: npm run build --prefix Hospi/pphfront
 
+# serveur express pour node
+# web: npm start
+
--- pphfront/server.js
+++ pphfront/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// Middleware de logging pour suivre les requêtes
+app.use((req, res, next) => {
+  console.log(`Request: ${req.method} ${req.url}`);
+  next();
 });
 
-const PORT = process.env.PORT || 8080;
+// Middleware pour servir les fichiers statiques depuis le dossier 'dist'
+app.use(express.static(path.resolve(__dirname, 'dist')));
+
+// Middleware pour gérer toutes les routes et renvoyer 'index.html'
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
