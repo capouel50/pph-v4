83681170cdc172d33f95901ca217a827930364a6
Jérémy <devpyth@outlook.fr>
2024-02-12 20:09:04 +0100

    v4

--- Hospi/settings.py
+++ Hospi/settings.py
@@ -72,7 +72,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [os.environ.get('MEMETRIA_REDIS_URL', 'redis://localhost:6379')],
         },
     },
 }
